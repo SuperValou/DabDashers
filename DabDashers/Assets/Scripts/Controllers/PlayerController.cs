cc7a3ca1ad32ee4e5a34c41eb5065ff947f77acc
SuperValou <valentin.esmieu@tuta.io>
2017-09-13 09:50:05 +0200

    Platforms and assets

---
+++ DabDashers/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Metronome Metronome;
+    public float DashForce = 1000;
+    public float JumpForce = 1000;
+    public float DefaultUpForce = 100;
+
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        this._rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private int lastBeat;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) &&
+            Metronome.BeatNumber > lastBeat &&
+            Metronome.BeatScore > 0)
+        {
+            lastBeat = Metronome.BeatNumber;
+            this._rigidbody.AddForce(new Vector2(DashForce, DefaultUpForce));
+        }
+    }
+}
--- DabDashers/Assets/Scripts/Metronome.cs
+++ DabDashers/Assets/Scripts/Metronome.cs
@@ -6,7 +6,8 @@ namespace Assets.Scripts
     public class Metronome : MonoBehaviour
     {
         private float _frequency;
-        
+        private float _threshold = 0.8f;
+
         public int BeatsPerMinute = 120;
         
         public float PiPhase = 1;
@@ -28,12 +29,11 @@ namespace Assets.Scripts
             BeatNumber = (int) Math.Floor(Source.time*_frequency);
 
             // set the score value of an input that would happen now
-            BeatScore = Mathf.Clamp(
-                (float)Math.Abs(Math.Cos(Math.PI*_frequency*Source.time + PiPhase*Math.PI)) - 0.8f, // |cos(pi * 2 * x + 0.5 * pi)| - 0.8
-                0f, 1f);
+            BeatScore = Mathf.Clamp01(
+                (float)Math.Abs(Math.Cos(Math.PI*_frequency*Source.time + PiPhase*Math.PI)) - _threshold // |cos(pi * 2 * x + 0.5 * pi)| - 0.8
+                );
             
             this.gameObject.transform.localScale = Vector3.one + Vector3.one * BeatScore;
-            
         }
     }
 }
