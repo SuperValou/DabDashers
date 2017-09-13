ed50c7c3b270f78bbe4d1db0384e5b637260f55d
SuperValou <valentin.esmieu@tuta.io>
2017-09-13 11:02:12 +0200

    Scenes setup

--- DabDashers/Assets/Scripts/Controllers/PlayerController.cs
+++ DabDashers/Assets/Scripts/Controllers/PlayerController.cs
@@ -3,28 +3,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody;
+
+    public int PlayerId = 1;
     public Metronome Metronome;
     public float DashForce = 1000;
     public float JumpForce = 1000;
     public float DefaultUpForce = 100;
-
-    private Rigidbody2D _rigidbody;
-
+    public float DefaultForwardForce = 100;
+    
     void Start()
     {
         this._rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+        if (Metronome == null)
+        {
+            Debug.LogError("You don't have set up the metronome!");
+        }
     }
 
     private int lastBeat;
 
     void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.Space) &&
+        if (Input.GetButtonDown("Dash_player" + PlayerId) &&
             Metronome.BeatNumber > lastBeat &&
             Metronome.BeatScore > 0)
         {
             lastBeat = Metronome.BeatNumber;
             this._rigidbody.AddForce(new Vector2(DashForce, DefaultUpForce));
+            return;
+        }
+        if (Input.GetButtonDown("Jump_player" + PlayerId) &&
+            Metronome.BeatNumber > lastBeat &&
+            Metronome.BeatScore > 0)
+        {
+            lastBeat = Metronome.BeatNumber;
+            this._rigidbody.AddForce(new Vector2(DefaultForwardForce, JumpForce));
+            return;
         }
     }
 }
---
+++ DabDashers/Assets/Scripts/DabState.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Scripts
+{
+    public enum PlayerState
+    {
+        Idle = 0,
+        Dabbing,
+        EndDabbing,
+        Wallowing,
+        JumpingUp,
+        JumpingDown,
+    }
+
+    public class StateMachine
+    {
+        
+    }
+}
