39c455f50a6270f391161e6cfcaf3a9cde3f543d
SuperValou <valentin.esmieu@tuta.io>
2017-09-14 20:26:58 +0200

    Loading message on main menu

--- DabDashers/Assets/Go.cs
+++ DabDashers/Assets/Go.cs
@@ -4,19 +4,20 @@ using Assets.Scripts.Levels;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Go : MonoBehaviour {
+public class Go : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject text;
+    public GameObject loading;
 	
 	// Update is called once per frame
 	void Update ()
     {
 	    if (Input.GetKeyDown(KeyCode.Return))
 	    {
-	        SceneManager.LoadScene((int) SceneIndex.Level1);
+	        text.SetActive(false);
+            loading.SetActive(true);
+            SceneManager.LoadScene((int)SceneIndex.Level1);
         }
 	}
 }
